3706349530fbd2de9bab788313a95bc82690e256
sai shravan <94429984+shravan81@users.noreply.github.com>
2023-06-23 21:59:27 +0530

    chore: use hero image with transparent background

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './hero.module.css'
 import Image from 'next/image'
-import sittingoncouch from 'public/illustrations/sittingoncouch.jpg'
+import hero from 'public/illustrations/hero.png'
 
 const Hero = () => {
   return (
@@ -11,14 +11,14 @@ const Hero = () => {
         id='home'
       >
         <div className={styles.info}>
-          <h2 className={styles.info__heading}>Hi 👋</h2>
+          <h2 className={styles.info__heading}>👋</h2>
           <h1 className={styles.info__subheading}>
             I am <span className={styles.info__highlight}>Sai Shravan</span>
           </h1>
         </div>
         <div className={styles.visuals}>
           <Image
-            src={sittingoncouch}
+            src={hero}
             alt='my headshot'
             priority
             className={styles.image}
