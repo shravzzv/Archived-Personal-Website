4a16e71497e09d528e22f0a698b5859192f35900
Sai Shravan <94429984+shravzzv@users.noreply.github.com>
2023-06-23 11:24:46 +0000

    styles: header with info and visual

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,21 +1,29 @@
 import React from 'react'
 import styles from './hero.module.css'
 import Image from 'next/image'
+import sittingoncouch from 'public/illustrations/sittingoncouch.jpg'
 
 const Hero = () => {
   return (
-    <div>
-      <div className={styles.container} id='home'>
-        <Image
-          src='/hero.jpg'
-          alt='hero'
-          width={100}
-          height={100}
-          className={styles.hero__image}
-        />
-        <h2>Hi 👋</h2>
-        <h1>I am Sai Shravan</h1>
-        <p>programmer and entrepreneur</p>
+    <div className={styles.hero}>
+      <div
+        className={`${styles.container} ${styles.hero__container}`}
+        id='home'
+      >
+        <div className={styles.info}>
+          <h2 className={styles.info__heading}>Hi 👋</h2>
+          <h1 className={styles.info__subheading}>
+            I am <span className={styles.info__highlight}>Sai Shravan</span>
+          </h1>
+        </div>
+        <div className={styles.visuals}>
+          <Image
+            src={sittingoncouch}
+            alt='my headshot'
+            priority
+            className={styles.image}
+          />
+        </div>
       </div>
     </div>
   )
