983030b328caad71af3469ac0687225302d51110
Sai Shravan <94429984+shravzzv@users.noreply.github.com>
2023-06-24 03:11:24 +0000

    styles: Add styles to contact section

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,46 +1,64 @@
+'use client'
 import React from 'react'
 import styles from './contact.module.css'
 
 const Contact = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const name: string = e.target[0].value
+    const email: string = e.target[1].value
+    const subject: string = e.target[2].value
+    const message: string = e.target[3].value
+    console.log(name, email, subject, message)
+  }
   return (
-    <div className={styles.container} id='contact'>
-      <h2 className={styles.contact__heading}>Contact Me</h2>
-      <p className={styles.contact__subheading}>Phone:</p>
-      <p className={styles.contact__info}>+91 insert non-personal phone here</p>
-      <p className={styles.contact__subheading}>Address:</p>
-      <p className={styles.contact__info}>Insert Address here</p>
-      <p className={styles.contact__subheading}>Email:</p>
-      <p className={styles.contact__info}>email@professional.com</p>
+    <div className={styles.contact}>
+      <div className={styles.container}>
+        <h2 className={styles.heading}>Contact</h2>
+        <div className={styles.contact__container}>
+          <div className={styles.info}>
+            <p className={styles.info__title}>Phone</p>
+            <p className={styles.info__content}>+91 9014857765</p>
+            <p className={styles.info__title}>Address</p>
+            <p className={styles.info__content}>
+              Chilkoor, Moinabad, ranga reddy
+            </p>
+            <p className={styles.info__title}>Email</p>
+            <p className={styles.info__content}>saishravancontact@gmail.com</p>
+          </div>
 
-      <form className={styles.contact__form}>
-        <input
-          type='text'
-          placeholder='Name'
-          className={styles.contact__form__input}
-        />
-        <input
-          type='email'
-          placeholder='Email'
-          className={styles.contact__form__input}
-        />
-        <input
-          type='text'
-          placeholder='Subject'
-          className={styles.contact__form__input}
-        />
-        <textarea
-          placeholder='Message'
-          className={styles.contact__form__input}
-          rows={8}
-        ></textarea>
-        <button
-          type='submit'
-          placeholder='message'
-          className={styles.contact__form__submit}
-        >
-          Send Message
-        </button>
-      </form>
+          <form className={styles.form} onSubmit={handleSubmit}>
+            <input
+              type='text'
+              name='name'
+              placeholder='Name'
+              className={styles.form__input}
+            />
+            <input
+              type='email'
+              name='email'
+              placeholder='Email'
+              className={styles.form__input}
+            />
+            <input
+              type='text'
+              name='subject'
+              placeholder='Subject'
+              className={styles.form__input}
+            />
+            <textarea
+              name='message'
+              placeholder='Message'
+              cols={30}
+              rows={10}
+              className={styles.form__input}
+            ></textarea>
+            <button type='submit' className={styles.form__submit}>
+              Send Message
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
