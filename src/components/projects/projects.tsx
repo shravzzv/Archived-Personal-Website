cf62d67a70618fe523b40c75ff14e84603a60c9a
Sai Shravan <94429984+shravzzv@users.noreply.github.com>
2023-07-11 10:04:42 +0000

    chore: update the portfolio section

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -15,34 +15,6 @@ const Projects = () => {
         <div className={styles.projects__container}>
           <div className={styles.project}>
             <Link
-              href='https://social-media-app-five-gamma.vercel.app/'
-              target='blank'
-              className={styles.imgContainer}
-            >
-              <Image
-                src={socialmediaproject}
-                alt='project name'
-                className={styles.project__image}
-              />
-            </Link>
-            <h3 className={styles.project__title}>Socialio</h3>
-            <p className={styles.project__desc}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Necessitatibus nulla magni a architecto alias saepe!
-            </p>
-            <div className={styles.project__socials}>
-              <Link href=''>
-                <Image
-                  src='/social media svgs/github.svg'
-                  alt='github'
-                  width={50}
-                  height={50}
-                />
-              </Link>
-            </div>
-          </div>
-          <div className={styles.project}>
-            <Link
               href='https://thefacebook.vercel.app/'
               target='blank'
               className={styles.imgContainer}
@@ -53,13 +25,105 @@ const Projects = () => {
                 className={styles.project__image}
               />
             </Link>
-            <h3 className={styles.project__title}>Thefacebook first version</h3>
+            <h3 className={styles.project__title}>Thefacebook 2004</h3>
             <p className={styles.project__desc}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Necessitatibus nulla magni a architecto alias saepe!
+              A MERN stack clone of the first version of Facebook.
             </p>
             <div className={styles.project__socials}>
-              <Link href=''>
+              <Link
+                href='https://github.com/shravzzv/thefacebook.comclient'
+                target='_blank'
+              >
+                <Image
+                  src='/social media svgs/github.svg'
+                  alt='github'
+                  width={50}
+                  height={50}
+                />
+              </Link>
+            </div>
+          </div>
+
+          <div className={styles.project}>
+            <Link
+              href='https://happinessproject.vercel.app/'
+              target='blank'
+              className={styles.imgContainer}
+            >
+              <Image
+                src={socialmediaproject}
+                alt='project name'
+                className={styles.project__image}
+              />
+            </Link>
+            <h3 className={styles.project__title}>The Happiness Project</h3>
+            <p className={styles.project__desc}>
+              An app that helps people prioritize happiness
+            </p>
+            <div className={styles.project__socials}>
+              <Link
+                href='https://github.com/shravzzv/thehappinessproject'
+                target='_blank'
+              >
+                <Image
+                  src='/social media svgs/github.svg'
+                  alt='github'
+                  width={50}
+                  height={50}
+                />
+              </Link>
+            </div>
+          </div>
+
+          <div className={styles.project}>
+            <Link
+              href='https://personalizeit.vercel.app'
+              target='blank'
+              className={styles.imgContainer}
+            >
+              <Image
+                src={thefacebookproject}
+                alt='project name'
+                className={styles.project__image}
+              />
+            </Link>
+            <h3 className={styles.project__title}>PersonalizeIt</h3>
+            <p className={styles.project__desc}>
+              An online store of quality productivity tools for knowledge
+              workers
+            </p>
+            <div className={styles.project__socials}>
+              <Link
+                href='https://github.com/shravzzv/templates'
+                target='_blank'
+              >
+                <Image
+                  src='/social media svgs/github.svg'
+                  alt='github'
+                  width={50}
+                  height={50}
+                />
+              </Link>
+            </div>
+          </div>
+
+          <div className={styles.project}>
+            <Link href='#' className={styles.imgContainer}>
+              <Image
+                src={socialmediaproject}
+                alt='this website'
+                className={styles.project__image}
+              />
+            </Link>
+            <h3 className={styles.project__title}>My Personal website</h3>
+            <p className={styles.project__desc}>
+              This website you're on, created using Next.js 13
+            </p>
+            <div className={styles.project__socials}>
+              <Link
+                href='https://github.com/shravzzv/my-portfolio'
+                target='_blank'
+              >
                 <Image
                   src='/social media svgs/github.svg'
                   alt='github'
@@ -76,3 +140,6 @@ const Projects = () => {
 }
 
 export default Projects
+
+// todo: style iframes
+// todo: flex wrap projects
