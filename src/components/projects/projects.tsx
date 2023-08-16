bf5d9546ab01b1dbf8b855a3fb325898ccda1c43
Sai Shravan Vadla <94429984+shravzzv@users.noreply.github.com>
2023-08-16 21:09:39 +0530

    feat(portfolio): remove The Happiness Project

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -18,37 +18,6 @@ const Projects = () => {
 
           <div className={styles.project}>
             <Link
-              href='https://happinessproject.vercel.app/'
-              target='blank'
-              className={styles.imgContainer}
-            >
-              <Image
-                src={happinessProject}
-                alt='project name'
-                className={styles.project__image}
-              />
-            </Link>
-            <h3 className={styles.project__title}>The Happiness Project</h3>
-            <p className={styles.project__desc}>
-              An app that helps people prioritize happiness
-            </p>
-            <div className={styles.project__socials}>
-              <Link
-                href='https://github.com/shravzzv/thehappinessproject'
-                target='_blank'
-              >
-                <Image
-                  src='/social media svgs/github.svg'
-                  alt='github'
-                  width={50}
-                  height={50}
-                />
-              </Link>
-            </div>
-          </div>
-
-          <div className={styles.project}>
-            <Link
               href='https://personalizeit.vercel.app'
               target='blank'
               className={styles.imgContainer}
